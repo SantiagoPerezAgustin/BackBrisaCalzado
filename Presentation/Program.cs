0d109c9690fe5e6c629f055d7bc8d11d00cd5e87
Santiago Perez <santiagus153@gmail.com>
2026-01-28 15:54:37 -0300

    Hice unos cambios en el progrma.cs

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -25,17 +25,10 @@ builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 // Configurar CORS para permitir solicitudes desde el frontend
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.WithOrigins(
-            "http://localhost:5173",    // Puerto por defecto de Vite
-            "http://localhost:3000",    // Otros puertos comunes
-            "http://localhost:5174"
-        )
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials();
-    });
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -45,7 +38,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Usar CORS antes de otros middlewares
-app.UseCors("AllowFrontend");
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
